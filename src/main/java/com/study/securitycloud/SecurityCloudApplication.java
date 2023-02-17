cf4bd697668b0d5edf36543d1d74bc9eca92c730
tlarbals824 <simkm0824@naver.com>
2023-02-17 10:51:50 +0900

    first commit

---
+++ src/main/java/com/study/securitycloud/SecurityCloudApplication.java
@@ -0,0 +1,13 @@
+package com.study.securitycloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/securitycloud/SecurityCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.securitycloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
