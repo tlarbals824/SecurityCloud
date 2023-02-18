b9d178b5b700b5a66c5921bde0820b302f0cdf46
tlarbals824 <simkm0824@naver.com>
2023-02-18 15:12:18 +0900

    Add Simple Main Page

--- src/main/java/com/study/securitycloud/user/controller/UserController.java
+++ src/main/java/com/study/securitycloud/user/controller/UserController.java
@@ -3,13 +3,21 @@ package com.study.securitycloud.user.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping(name = "/")
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/")
 public class UserController {
 
-	@GetMapping
+	@ResponseBody
+	@RequestMapping(value = "main", method = RequestMethod.GET)
 	public String mainPage(){
-		return "hi";
+		log.info("access success");
+		return "hello!";
 	}
 }
